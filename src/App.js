789b79b668ac21151288913ddb97f2a8ba2ae717
Gustavo Farias <gustavogarcia266@hotmail.com>
2022-07-02 21:58:53 -0300

    feat: values

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GlobalStyle from "./styles/global";
 import Header from "./components/Header";
 import Resume  from "./components/resume";
 import Form from "./components/Form";
 
 const App = () => {
+  const data = localStorage.getItem("transactions");
+  const [transactionsList, setTransactionsList] = useState(
+    data ? JSON.parse(data) : []
+  );
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const amountExpense = transactionsList
+    .filter((item) => item.expense)
+    .map((transaction) => Number(transaction.amount))
+
+    const amountIncome = transactionsList
+    .filter((item) => !item.expense)
+    .map((transaction) => Number(transaction.amount));
+
+    const expense = amountExpense.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+    const income = amountIncome.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+
+    const total = Math.abs(income - expense).toFixed(2);
+
+    setIncome(`R$ ${income}`);
+    setExpense(`R$ ${expense}`);
+
+    setTotal(`${Number(income) < Number(expense) ? "-" : ""}R$ ${total}`);
+  }, [transactionsList])
+
   return (
     <>
       <Header />
-      <Resume />
+      <Resume income={income} expense={expense} total={total}/>
       <GlobalStyle />
       <Form></Form>
     </>
--- src/components/resume/index.js
+++ src/components/resume/index.js
@@ -7,13 +7,13 @@ import {
     FaDollarSign,
 } from "react-icons/fa";
 
-const Resume = () => {
+const Resume = ({ income, expense, total }) => {
   return (
     <>
     <C.Container>
-        <ResumeItem title="Entradas" value={1000} Icon={FaRegArrowAltCircleUp}/>
-        <ResumeItem title="Saídas" value={1000} Icon={FaRegArrowAltCircleDown}/>
-        <ResumeItem title="Total" value={1000} Icon={FaDollarSign}/>
+        <ResumeItem title="Entradas" value={income} Icon={FaRegArrowAltCircleUp}/>
+        <ResumeItem title="Saídas" value={expense} Icon={FaRegArrowAltCircleDown}/>
+        <ResumeItem title="Total" value={total} Icon={FaDollarSign}/>
     </C.Container>
     </>
   )
